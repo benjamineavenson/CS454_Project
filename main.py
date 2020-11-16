1c09bf72ee4323684193978cb889b5f87cc34272
benjamineavenson <eavensonbenjamin@gmail.com>
2020-11-16 15:42:57 -0800

    created landing and advanced search pages

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('landing_page.html')
 
 @app.route('/test')
 def test():
     return render_template('template.html')
+
+@app.route('/advanced_search')
+def advanced():
+    return render_template('advanced.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
